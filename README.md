78257519f2b87213557d90d8fddebaf8a0af0a3c
Christopher Giordano <chrisgcpg@gmail.com>
2024-04-27 19:15:54 -0400

    Added nav links in the table of contents. Changed the way the prompts are worded. Fixed the writeFile function.

--- README.md
+++ README.md
@@ -1,27 +1,45 @@
-# Title
-
-## Description
+# sdasdasdas
 
 ## Table of Contents
-(nav links to each section of this README)
-## Installation
+* [Description](#description)
+* [Technology Used](#technology-used)
+* [Installation](#installation)
+* [Usage Information](#usage-information)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+* [Github Profile](#github)
+* [E-mail](#email)
 
-## Usage Information
+## Description <a id="description"></a>
+asdsadas
 
-## License
+## Technology Used <a id="technology-used"></a>
+aasdfasdf
+
+## Installation <a id="installation"></a>
+asdfasdf
+
+## Usage Information <a id="usage-information"></a>
+How the user will use the application and functionality of all the features.
+asdf
+
+## License <a id="license"></a>
 (WHEN I choose a license for my application from a list of options
 THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under)
-## Contributing
+
+## Contributing <a id="contributing"></a>
 (contributing guidelines)
 
-## Tests
-(test instructions)
+## Tests <a id="tests"></a>
+Preform tests on the application and report any bugs to the developer.
+asdf
 
-## Questions
-### User's Github Profile  
-(Link to User's Github Profile)
-### User's E-mail
-(user's E-mail)
-
-
-
+## Questions <a id="questions"></a>
+You can report any issues in the issues tab on Github. Or you can send any questions to the e-mail provided. 
+Be clear with what repository you are referring to and as much detail as posible.
+### Github Profile <a id="github"></a>
+[asdf's Github Profile](https://github.com/asdf)
+### E-mail <a id="email"></a>
+asdf
--- index.js
+++ index.js
@@ -6,36 +6,47 @@ const { type } = require('os');
 const { title } = require('process');
 
 // TODO: Create an array of questions for user input to access later with inquirer
+// Added prompts in the messages along with clairity of where the inputs will go and what what they will look like.
 const questions = [
     {
     type: "input",
-    message: "Enter title of the website/application." ,
+    message: "Enter title of the website/application. This will display at the top of the readme." ,
     name: 'title'
 },
     {
     type: "input",
-    message: "Enter a description of the website/application." ,
+    message: "Enter a concept or brief description of the website/application. This will display below the Description header." ,
     name: 'description'
 },
     {
     type: "input",
-    message: "Enter directions for installation or other programs needed to run.",
+    message: "Enter the technology used in the making and running of the website/application. This will display below the Technology header." ,
+    name: 'technology'
+},
+    {
+    type: "input",
+    message: "Enter directions for installation or other programs needed to run. This will display below the Installation header.",
     name:'installation'
 },
     {
     type: "input",
-    message: "Enter all of the functionality of the website or application and how to navigate it." ,
+    message: "Enter all of the functionality of the website or application and how to navigate it. This will display below the Usage header." ,
     name: 'usage'
 },
     {
     type: "input",
-    message: "Enter contributors and how the user can contribute if possible." ,
+    message: "Enter contributors and how the user can contribute if possible. This will display below the Contributing header." ,
     name: 'contributing'
 },
     {
     type: "input",
-    message: "Enter how the user can set up tests, run them, and interpreting the results" ,
+    message: "Enter how the user can set up tests, run them, and interpreting the results. This will display below the Tests header." ,
     name: 'tests'
+},
+    {
+    type: "input",
+    message: "Enter future development ideas if given more time or resourses. This will display below the Future Developement header." ,
+    name: 'futureDevelopement'
 },
     {
     type: "input",
@@ -50,33 +61,52 @@ const questions = [
 
 ];
 
-// Outline variable of README
+// Outline variable of README with nav links and template literals for the inputs in the terminal.
 const generateReadMe = (response) =>
 `# ${response.title}
 
-## Description
+## Table of Contents
+* [Description](#description)
+* [Technology Used](#technology-used)
+* [Installation](#installation)
+* [Usage Information](#usage-information)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+* [Github Profile](#github)
+* [E-mail](#email)
+
+## Description <a id="description"></a>
 ${response.description}
 
-## Table of Contents
-(nav links to each section of this README)
-## Installation
+## Technology Used <a id="technology-used"></a>
+${response.technology}
+
+## Installation <a id="installation"></a>
 ${response.installation}
 
-## Usage Information
+## Usage Information <a id="usage-information"></a>
+How the user will use the application and functionality of all the features.
 ${response.usage}
-## License
+
+## License <a id="license"></a>
 (WHEN I choose a license for my application from a list of options
 THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under)
-## Contributing
+
+## Contributing <a id="contributing"></a>
 (contributing guidelines)
 
-## Tests
+## Tests <a id="tests"></a>
+Preform tests on the application and report any bugs to the developer.
 ${response.tests}
 
-## Questions
-### Github Profile  
-['${response.github}'s Github Profile'](https://github.com/${response.github}'${response.github}'s Github Profile')
-### E-mail
+## Questions <a id="questions"></a>
+You can report any issues in the issues tab on Github. Or you can send any questions to the e-mail provided. 
+Be clear with what repository you are referring to and as much detail as posible.
+### Github Profile <a id="github"></a>
+[${response.github}'s Github Profile](https://github.com/${response.github})
+### E-mail <a id="email"></a>
 ${response.email}`
 
 // Prompt with questions and return readme with input values.
@@ -86,7 +116,7 @@ inquirer
     const readMePageContent = generateReadMe(response);
     
     // TODO: Create a function to write README file
-    fs.writeToFile('README.md', readMePageContent, (err) =>
+    fs.writeFile('README.md', readMePageContent, (err) =>
     err ? console.log(err) : console.log('Successfully created README.md file')
     );
 });
@@ -94,7 +124,7 @@ inquirer
 
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {}https://www.twitch.tv/justchri5
 
 // Function call to initialize app
 init();
