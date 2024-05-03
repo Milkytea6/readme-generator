c1e142ff3ca772a63565adcca1b6817206e1ec10
Christopher Giordano <chrisgcpg@gmail.com>
2024-05-03 19:13:43 -0400

    Added license badge and link to the readme via node.js. Also moved some functions around to export to the file that was calling generateReadMe and fixed the way i was inporting the function as well.

--- README.md
+++ README.md
@@ -1,45 +1,46 @@
-# sdasdasdas
-
-## Table of Contents
-* [Description](#description)
-* [Technology Used](#technology-used)
-* [Installation](#installation)
-* [Usage Information](#usage-information)
-* [License](#license)
-* [Contributing](#contributing)
-* [Tests](#tests)
-* [Questions](#questions)
-* [Github Profile](#github)
-* [E-mail](#email)
-
-## Description <a id="description"></a>
-asdsadas
-
-## Technology Used <a id="technology-used"></a>
-aasdfasdf
-
-## Installation <a id="installation"></a>
-asdfasdf
-
-## Usage Information <a id="usage-information"></a>
-How the user will use the application and functionality of all the features.
-asdf
-
-## License <a id="license"></a>
-(WHEN I choose a license for my application from a list of options
-THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under)
-
-## Contributing <a id="contributing"></a>
-(contributing guidelines)
-
-## Tests <a id="tests"></a>
-Preform tests on the application and report any bugs to the developer.
-asdf
-
-## Questions <a id="questions"></a>
-You can report any issues in the issues tab on Github. Or you can send any questions to the e-mail provided. 
-Be clear with what repository you are referring to and as much detail as posible.
-### Github Profile <a id="github"></a>
-[asdf's Github Profile](https://github.com/asdf)
-### E-mail <a id="email"></a>
-asdf
+# 1234
+  
+  ## Table of Contents
+  * [Description](#description)
+  * [Technology Used](#technology-used)
+  * [Installation](#installation)
+  * [Usage Information](#usage-information)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  * [Github Profile](#github)
+  * [E-mail](#email)
+  
+  ## Description <a id="description"></a>
+  1234
+  
+  ## Technology Used <a id="technology-used"></a>
+  HTML,CSS,Javascript,API's,third-party API's,server-side API's,node.js,OOP,Express.js
+  
+  ## Installation <a id="installation"></a>
+  install
+  
+  ## Usage Information <a id="usage-information"></a>
+  How the user will use the application and functionality of all the features.
+  fgunc
+  
+  ## License <a id="license"></a>
+  ### Apache
+  * ![Static Badge](https://img.shields.io/badge/Apache-Badge-red?style=flat)
+  * [Link to License Page](http://www.apache.org/licenses/LICENSE-2.0)
+  
+  ## Contributing <a id="contributing"></a>
+  contrib
+  
+  ## Tests <a id="tests"></a>
+  Preform tests on the application and report any bugs to the developer.
+  tests
+  
+  ## Questions <a id="questions"></a>
+  You can report any issues in the issues tab on Github. Or you can send any questions to the e-mail provided. 
+  Be clear with what repository you are referring to and as much detail as posible.
+  ### Github Profile <a id="github"></a>
+  [milkytea6's Github Profile](https://github.com/milkytea6)
+  ### E-mail <a id="email"></a>
+  chris
--- index.js
+++ index.js
@@ -4,127 +4,88 @@ const inquirer = require('inquirer');
 const colors = require('colors');
 const { type } = require('os');
 const { title } = require('process');
+const { default: Choice } = require('inquirer/lib/objects/choice');
+const { default: Choices } = require('inquirer/lib/objects/choices');
+const generateReadMe = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input to access later with inquirer
 // Added prompts in the messages along with clairity of where the inputs will go and what what they will look like.
 const questions = [
     {
-    type: "input",
-    message: "Enter title of the website/application. This will display at the top of the readme." ,
-    name: 'title'
-},
+        type: "input",
+        message: "Enter title of the website/application. This will display at the top of the readme.",
+        name: 'title'
+    },
     {
-    type: "input",
-    message: "Enter a concept or brief description of the website/application. This will display below the Description header." ,
-    name: 'description'
-},
+        type: "input",
+        message: "Enter a concept or brief description of the website/application. This will display below the Description header.",
+        name: 'description'
+    },
+    // Change to list type and added choices
     {
-    type: "input",
-    message: "Enter the technology used in the making and running of the website/application. This will display below the Technology header." ,
-    name: 'technology'
-},
+        type: "checkbox",
+        message: "Enter the technology used in the making and running of the website/application. This will display below the Technology header.",
+        name: 'technology',
+        choices: ["HTML", "CSS", "Javascript", "API's", "Third-Party API's", "Server-Side API's", "Node.js", "OOP", "Express.js"]
+    },
     {
-    type: "input",
-    message: "Enter directions for installation or other programs needed to run. This will display below the Installation header.",
-    name:'installation'
-},
+        type: "input",
+        message: "Enter directions for installation or other programs needed to run. This will display below the Installation header.",
+        name: 'installation',
+    },
     {
-    type: "input",
-    message: "Enter all of the functionality of the website or application and how to navigate it. This will display below the Usage header." ,
-    name: 'usage'
-},
+        type: "input",
+        message: "Enter all of the functionality of the website or application and how to navigate it. This will display below the Usage header.",
+        name: 'usage'
+    },
     {
-    type: "input",
-    message: "Enter contributors and how the user can contribute if possible. This will display below the Contributing header." ,
-    name: 'contributing'
-},
+        type: 'list',
+        message: 'Choose a license from the list provided.',
+        name: 'license',
+        choices: ['MIT', 'Apache', 'No License']
+    },
     {
-    type: "input",
-    message: "Enter how the user can set up tests, run them, and interpreting the results. This will display below the Tests header." ,
-    name: 'tests'
-},
+        type: "input",
+        message: "Enter contributors and how the user can contribute if possible. This will display below the Contributing header.",
+        name: 'contributing'
+    },
     {
-    type: "input",
-    message: "Enter future development ideas if given more time or resourses. This will display below the Future Developement header." ,
-    name: 'futureDevelopement'
-},
+        type: "input",
+        message: "Enter how the user can set up tests, run them, and interpreting the results. This will display below the Tests header.",
+        name: 'tests'
+    },
     {
-    type: "input",
-    message: "Enter Github username for link to profile.",
-    name: 'github'
-},
+        type: "input",
+        message: "Enter future development ideas if given more time or resourses. This will display below the Future Developement header.",
+        name: 'futureDevelopement'
+    },
     {
-    type: "input",
-    message:  "Enter E-mail for the user to contact the developer.",
-    name: 'email'
-}
+        type: "input",
+        message: "Enter Github username for link to profile.",
+        name: 'github'
+    },
+    {
+        type: "input",
+        message: "Enter E-mail for the user to contact the developer.",
+        name: 'email'
+    }
 
 ];
 
-// Outline variable of README with nav links and template literals for the inputs in the terminal.
-const generateReadMe = (response) =>
-`# ${response.title}
-
-## Table of Contents
-* [Description](#description)
-* [Technology Used](#technology-used)
-* [Installation](#installation)
-* [Usage Information](#usage-information)
-* [License](#license)
-* [Contributing](#contributing)
-* [Tests](#tests)
-* [Questions](#questions)
-* [Github Profile](#github)
-* [E-mail](#email)
-
-## Description <a id="description"></a>
-${response.description}
-
-## Technology Used <a id="technology-used"></a>
-${response.technology}
-
-## Installation <a id="installation"></a>
-${response.installation}
-
-## Usage Information <a id="usage-information"></a>
-How the user will use the application and functionality of all the features.
-${response.usage}
-
-## License <a id="license"></a>
-(WHEN I choose a license for my application from a list of options
-THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under)
-
-## Contributing <a id="contributing"></a>
-(contributing guidelines)
-
-## Tests <a id="tests"></a>
-Preform tests on the application and report any bugs to the developer.
-${response.tests}
-
-## Questions <a id="questions"></a>
-You can report any issues in the issues tab on Github. Or you can send any questions to the e-mail provided. 
-Be clear with what repository you are referring to and as much detail as posible.
-### Github Profile <a id="github"></a>
-[${response.github}'s Github Profile](https://github.com/${response.github})
-### E-mail <a id="email"></a>
-${response.email}`
-
-// Prompt with questions and return readme with input values.
-inquirer
-.prompt(questions)
-.then((response) => {
-    const readMePageContent = generateReadMe(response);
-    
-    // TODO: Create a function to write README file
-    fs.writeFile('README.md', readMePageContent, (err) =>
-    err ? console.log(err) : console.log('Successfully created README.md file')
-    );
-});
-
-
-
 // TODO: Create a function to initialize app
-function init() {}https://www.twitch.tv/justchri5
+// Prompt with questions and return readme with input values.
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((response) => {
+            const readMePageContent = generateReadMe(response);
+
+            // TODO: Create a function to write README file
+            fs.writeFile('README.md', readMePageContent, (err) =>
+                err ? console.log(err) : console.log('Successfully created README.md file')
+            );
+        });
+}
 
 // Function call to initialize app
 init();
